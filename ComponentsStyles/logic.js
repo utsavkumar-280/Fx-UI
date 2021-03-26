0a31bcad517b815f797ed6ef59d4edad27588405
Utsav Kumar <utsavkumar280@gmail.com>
2021-03-26 15:23:22 +0530

    styles: added toast styles

---
+++ ComponentsStyles/logic.js
@@ -0,0 +1,42 @@
+//toast logic
+let successVar = document.querySelector(".success");
+let warningVar = document.querySelector(".warning");
+let errorVar = document.querySelector(".error");
+
+const Toast = {
+  init() {
+    this.hideTimeout = null;
+
+    this.el = document.createElement("div");
+    this.el.className = "toast";
+    document.body.appendChild(this.el);
+  },
+
+  show(message, state) {
+    clearTimeout(this.hideTimeout);
+
+    this.el.textContent = message;
+    this.el.className = "toast toastVisible";
+
+    if (state) {
+      this.el.classList.add(`toast${state}`);
+    }
+
+    this.hideTimeout = setTimeout(() => {
+      this.el.classList.remove("toastVisible");
+    }, 2000);
+  },
+};
+
+document.addEventListener("DOMContentLoaded", () => Toast.init());
+successVar.addEventListener("click", () =>
+  Toast.show("success message !!", "Success")
+);
+
+warningVar.addEventListener("click", () =>
+  Toast.show("warning message  !!", "Warning")
+);
+
+errorVar.addEventListener("click", () =>
+  Toast.show("error message  !!", "Error")
+);
