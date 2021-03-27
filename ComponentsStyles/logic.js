e9ef50211cb4263053c1ef080e394c1d5fd70a87
Utsav Kumar <utsavkumar280@gmail.com>
2021-03-27 16:04:31 +0530

    refact: changed carousel

--- ComponentsStyles/logic.js
+++ ComponentsStyles/logic.js
@@ -48,14 +48,40 @@ deleteMessg.addEventListener("click", () => {
 });
 
 // Carousel Logic
-var counter = 1;
-setInterval(function () {
-  document.getElementById("radio" + counter).checked = true;
-  counter++;
-  if (counter > 4) {
-    counter = 1;
-  }
-}, 5000);
+// var counter = 1;
+// setInterval(function () {
+//   document.getElementById("radio" + counter).checked = true;
+//   counter++;
+//   if (counter > 4) {
+//     counter = 1;
+//   }
+// }, 5000);
+
+//New Carousel
+const carouselImages = document.querySelector(".carousel__images");
+const carouselButtons = document.querySelectorAll(".carousel__button");
+const numberOfImages = document.querySelectorAll(".carousel__images img")
+  .length;
+let imageIndex = 1;
+let translateX = 0;
+
+carouselButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    if (event.target.id === "previous") {
+      if (imageIndex !== 1) {
+        imageIndex--;
+        translateX += 300;
+      }
+    } else {
+      if (imageIndex !== numberOfImages) {
+        imageIndex++;
+        translateX -= 300;
+      }
+    }
+
+    carouselImages.style.transform = `translateX(${translateX}px)`;
+  });
+});
 
 //Modal Button
 var modalTrig = document.querySelector(".modalTrigger");
